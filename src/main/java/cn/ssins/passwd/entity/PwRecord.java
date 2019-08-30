11b1d8764401fe80716814bb28578d620888a1a9
ssins <781537596@qq.com>
2019-08-30 18:05:14 +0800

    init

--- src/main/java/cn/ssins/passwd/entity/PwRecord.java
+++ src/main/java/cn/ssins/passwd/entity/PwRecord.java
@@ -25,6 +25,9 @@ public class PwRecord extends Model<PwRecord> {
     @TableField("host")
     private String host;
 
+    @TableField("keywords")
+    private String keywords;
+
     @TableField("hits")
     private Integer hits;
 
@@ -32,52 +35,69 @@ public class PwRecord extends Model<PwRecord> {
         return id;
     }
 
-    public void setId(String id) {
+    public PwRecord setId(String id) {
         this.id = id;
+        return this;
     }
 
     public String getKeyId() {
         return keyId;
     }
 
-    public void setKeyId(String keyId) {
+    public PwRecord setKeyId(String keyId) {
         this.keyId = keyId;
+        return this;
     }
 
     public String getUsername() {
         return username;
     }
 
-    public void setUsername(String username) {
+    public PwRecord setUsername(String username) {
         this.username = username;
+        return this;
     }
 
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password) {
+    public PwRecord setPassword(String password) {
         this.password = password;
+        return this;
     }
 
     public String getHost() {
         return host;
     }
 
-    public void setHost(String host) {
+    public PwRecord setHost(String host) {
         this.host = host;
+        return this;
     }
 
     public Integer getHits() {
         return hits;
     }
 
-    public void setHits(Integer hits) {
+    public PwRecord setHits(Integer hits) {
         this.hits = hits;
+        return this;
+    }
+
+    public String getKeywords() {
+        return keywords;
+    }
+
+    public PwRecord setKeywords(String keywords) {
+        this.keywords = keywords;
+        return this;
     }
 
     @Override
     protected Serializable pkVal() {
         return this.id;
     }
+
+
 }
--- src/main/java/cn/ssins/passwd/web/PwMainController.java
+++ src/main/java/cn/ssins/passwd/web/PwMainController.java
@@ -1,6 +1,7 @@
 package cn.ssins.passwd.web;
 
 import cn.hextec.common.upms.ResultT;
+import cn.ssins.passwd.entity.PwKey;
 import cn.ssins.passwd.entity.PwRecord;
 import cn.ssins.passwd.service.IPwKeyService;
 import cn.ssins.passwd.service.IPwRecordService;
@@ -42,5 +43,31 @@ public class PwMainController {
             return ResultT.success(pwRecord);
         }
         return ResultT.ex("未查询到符合的记录");
+    }
+
+    @ApiOperation(value = "注册，提供公钥")
+    @RequestMapping(value = "register", method = RequestMethod.POST)
+    public ResultT<Boolean> register(@ApiParam(name = "publicKey", value = "公钥") @RequestParam String publicKey) {
+        PwKey pwKey = iPwKeyService.getOne(new QueryWrapper<PwKey>().eq("public_key", publicKey));
+        if (pwKey != null) {
+            return ResultT.ex("注册失败");
+        }
+        pwKey = new PwKey().setPublicKey(publicKey).setValid("1");
+        return ResultT.success(iPwKeyService.save(pwKey));
+    }
+
+    @ApiOperation(value = "新增记录")
+    @RequestMapping(value = "addRecord", method = RequestMethod.POST)
+    public ResultT<Boolean> addRecord(@ApiParam(name = "publicKey", value = "公钥") @RequestParam String publicKey,
+                                      @ApiParam(name = "host", value = "域名") @RequestParam String host,
+                                      @ApiParam(name = "username", value = "用户名") @RequestParam String username,
+                                      @ApiParam(name = "password", value = "密码") @RequestParam String password,
+                                      @ApiParam(name = "keywords",value = "关键词")@RequestParam String keywords) {
+        PwKey pwKey = iPwKeyService.getOne(new QueryWrapper<PwKey>().eq("public_key", publicKey));
+        if (pwKey == null) {
+            return ResultT.ex("用户不存在");
+        }
+        PwRecord pwRecord = new PwRecord().setKeyId(pwKey.getId()).setHost(host).setPassword(password).setUsername(username).setKeywords(keywords);
+        return ResultT.success(iPwRecordService.save(pwRecord));
     }
 }
